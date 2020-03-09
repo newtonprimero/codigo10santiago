1f0147d04c6881e5084b7b8806d6c928c255f647
Gustavo <sant199_@hotmail.com>
2020-03-09 18:04:46 -0500

    semana2-dia1

---
+++ Semana 2/Dia 1/Ejemplo con float y position/.java
@@ -0,0 +1,19 @@
+//
+public class Saludo
+
+{
+
+public static void main( String[] args)
+
+{
+
+System.out.println("Saludos desde ");
+
+ 
+
+//Fin del metodo main
+
+  }
+
+ }
+
---
+++ backend/psint/22.psc
@@ -0,0 +1,9 @@
+Algoritmo sin_titulo
+	Escribir "ingrese cantidad de minutos"
+	Leer m
+	Escribir "valor por minuto"
+	leer v
+	total=m*v
+	Escribir "total",total
+	
+FinAlgoritmo
